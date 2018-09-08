11bad836c0acc044f04baa2c0dd1abcbb56ce7cf
Tanmay Thakur <thakurtanmay72@yahoo.com>
2018-09-09 00:14:14 +0530

    Updated model

--- wehack.py
+++ wehack.py
@@ -8,14 +8,15 @@ import numpy as np
 import pandas as pd
 import os
 
-os.chdir("C:\\Users\tanma\Desktop\Wehack 2.0")
+
+#os.chdir("C:\\Users\tanma\Desktop\Wehack 2.0")
 cwd = os.getcwd()
 print (cwd)
 
 data = pd.read_csv("Hack_A_Thon_DataSet_Rev1.csv")
 data['quality_per_artifact'] = 1/(data["consistency in load"]*data["consistency in quality"])
-#mask = data['duration(days)']<=0
-#data[mask] = np.mean(data["duration(days)"])
+mask = data['duration(days)']<=0
+data = data.drop(data[mask].index)
 #data['prev_month_art'] = data['#of artifacts']-data["artifact trend"]
 data = data.drop(["complexity"], axis = 1)
 
@@ -23,7 +24,6 @@ x = data.drop('total defects count', axis = 1)
 y = data["total defects count"]
 
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 x_train,x_cv,y_train,y_cv = train_test_split(x_train,y_train,test_size=0.25,random_state=42)
 
@@ -42,3 +42,4 @@ gbr = GridSearchCV(pip_lasso,param_grid).fit(x_train,y_train)
 print(mean_squared_error(gbr.predict(x_cv),y_cv))
 print(gbr.best_score_)
 print(gbr.best_params_)
+
